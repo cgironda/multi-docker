222aa1af23616d17220b0258a86f0b01c378de53
Carlos Gironda <cgironda@akane.waseda.jp>
2019-05-16 15:19:00 +0900

    Updated files

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to KM - Fibonacci Numbers</h1>
+            <h1 className="App-title">Fibonacci Numbers</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
